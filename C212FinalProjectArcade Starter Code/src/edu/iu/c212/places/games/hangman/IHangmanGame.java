c5af6170b0a7a4a204f9dc39718307075e75947a
Mitchell Kelly <indymickey@gmail.com>
2021-12-09 16:18:21 -0500

    NumberGuess and Hangman games

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/hangman/IHangmanGame.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/hangman/IHangmanGame.java
@@ -1,4 +1,9 @@
 package edu.iu.c212.places.games.hangman;
 
+import java.util.List;
+
 public interface IHangmanGame {
+    String getBlurredWord(List<Character> guesses, String word);
+
+    List<Character> getValidLexicon();
 }
