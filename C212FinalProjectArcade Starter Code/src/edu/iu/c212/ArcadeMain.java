07662dbdb3f96bceafd503461639842736c558b1
David Resinos <resinosdavid@gmail.com>
2021-12-09 18:33:53 -0500

    Added Group Names and Number

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/ArcadeMain.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/ArcadeMain.java
@@ -1,5 +1,14 @@
 package edu.iu.c212;
 
+/*
+    C212 Final Project
+    Group #14
+    David Resinos
+    Kyle Liss 
+    Mitchell Kelly 
+    Zach Watson
+ */
+
 public class ArcadeMain {
     public static void main(String[] args) {
         Arcade DaveAndBusters = new Arcade();
